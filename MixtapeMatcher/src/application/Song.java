9b82cb343d7b31f8743215883f791bc60f460a67
Bridget Winn <bmwinn@calpoly.edu>
2017-11-05 17:42:39 -0800

    Initial commit

---
+++ MixtapeMatcher/src/application/Song.java
@@ -0,0 +1,19 @@
+
+public class Song {
+	private String title;
+	private String artist;
+	private float length;
+	
+	public Song() {}
+	
+	public Song(String title, String artist) {
+		this.title = title;
+		this.artist = artist;
+	}
+	
+	public Song(String title, String artist, float length) {
+		this.title = title;
+		this.artist = artist;
+		this.length = length;
+	}
+}
