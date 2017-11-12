7a613209976d0cb0340c538cdf9411926611c392
Bridget Winn <bmwinn@calpoly.edu>
2017-11-12 15:22:54 -0800

    Added functionality to playlist

--- MixtapeMatcher/src/application/Playlist.java
+++ MixtapeMatcher/src/application/Playlist.java
@@ -8,8 +8,23 @@ public class Playlist {
 	
 	public Playlist(String name) {
 		this.name = name;
-		}
+	}
 	
 	public ArrayList<Song> getSongs() { return songs; }
 	public String getName() { return name; }
+	
+
+	// change accordingly
+	public void addSong(String title, String artist) {
+		Song addition = new Song(title, artist);
+		songs.add(addition);
+	}
+
+	public void deleteSong(String title, artist) {
+		for (Song s : songs) {
+			if (s.getTitle().equals(title) && s.getArtist().equals(artist))
+				songs.remove(s);
+		}
+	}
+	
 }
