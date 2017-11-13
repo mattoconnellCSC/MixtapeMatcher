59228ab9babba56031092ba80c466005ce46f50b
Bridget Winn <bmwinn@calpoly.edu>
2017-11-13 09:26:34 -0800

    Added tests for Playlist

---
+++ MixtapeMatcher/src/test/TestPlaylist.java
@@ -0,0 +1,48 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import application.Playlist;
+import application.Song;
+
+public class TestPlaylist {
+
+	@Test
+	public void testGetName() {
+		Playlist mine = new Playlist("My Playlist");
+		
+		assertEquals(mine.getName(), "My Playlist");
+	}
+	
+	@Test
+	public void testGetSongs() {
+		Playlist mine = new Playlist("My Playlist");
+
+		mine.addSong("Help", "Beatles");
+		mine.addSong("Waves",  "Electric Guest");
+		
+		assertEquals(mine.isEmpty(), false);
+	}
+	
+	@Test
+	public void testDeleteSong() {
+		Playlist mine = new Playlist("My Playlist");
+
+		mine.addSong("Help", "Beatles");
+		mine.deleteSong("Help",  "Beatles");
+		
+		assertEquals(mine.isEmpty(), true);
+	}
+
+	
+	@Test
+	public void testGetCurrentSong() {
+		Playlist mine = new Playlist("My Playlist");
+		Song song1 = new Song("Stayin' Alive", "Bee Gees");
+		mine.addSong(song1);
+		
+		assertEquals(mine.getCurrentSong(), song1);
+	}
+}
