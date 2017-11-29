50a8db3362f25bac398d8c2f31c8c4592bb5cd6c
Bridget Winn <bmwinn@calpoly.edu>
2017-11-28 19:33:05 -0800

    Lobby revised since networking fantasy

--- MixtapeMatcher/src/application/Lobby.java
+++ MixtapeMatcher/src/application/Lobby.java
@@ -9,37 +9,12 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class Lobby {
-	private String name;
 	private ArrayList<Player> players;
-	private Hashtable<String, Integer> gameStatus; // 0 not done, 1 done
-	private boolean gameOver = false;
 	public int maxSongs;
 	
-	public Lobby(String name, int maxSongs) {
-		this.name = name;
-		this.maxSongs = maxSongs;
+	public Lobby() {
 		players = new ArrayList<Player>();
-		gameStatus = new Hashtable<String, Integer>();
 	}
-
-	public void updatePlayerStatus(String pname, Integer status) {
-		gameStatus.put(pname, status);
-	}
-	
-	public void setGameOver() {
-		for (Player p : players) {
-			String pname = p.getName();
-			
-			if (gameStatus.get(pname) == 0) {
-				gameOver = false;
-				break;
-			}
-		}
-		
-		gameOver = true;
-	}
-	
-	public String getName() { return name; }
 	
 	public int getMaxSongs() {
 		return maxSongs;
@@ -47,19 +22,10 @@ public class Lobby {
 	
 	public void addPlayer(Player p) {
 		players.add(p);
-		return;
-	}
-	
-	public Player getPlayer(String uniquePlayerName) {
-		//query database of players for player will unique name
-		//return player
-		return null;
 	}
 	
 	public ArrayList<Player> getPlayerList() {
 		return players;
 	}
 	
-	/*public String getWinner() {
-	} */
 }
