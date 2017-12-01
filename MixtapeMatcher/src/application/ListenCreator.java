18fe17d001cedef89a5a62977ae53386c0e36722
Tanay Gottigundala <tgottigundala@gmail.com>
2017-11-30 19:59:28 -0800

    Listen Page style complete

--- MixtapeMatcher/src/application/ListenCreator.java
+++ MixtapeMatcher/src/application/ListenCreator.java
@@ -4,9 +4,12 @@ import java.net.URL;
 import java.util.ArrayList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -38,6 +41,17 @@ public class ListenCreator extends SceneCreator {
 	
 	public Scene createScene(Stage stage) {
 		VBox root = new VBox();
+		
+		Label titleText = new Label("Listen to Playlist");
+		titleText.getStyleClass().add("label-title");
+		titleText.setAlignment(Pos.CENTER);
+		VBox titleBox = new VBox();
+		titleBox.setPadding(new Insets(15, 10, 10, 10));
+		titleBox.getChildren().add(titleText);
+		titleBox.setAlignment(Pos.CENTER_LEFT);
+		
+		root.getChildren().add(titleText);
+		
 		Scene scene = new Scene(root);
 	  
 		Image playImg = new Image(getClass().getResourceAsStream("../resources/Play.png"));
@@ -52,13 +66,15 @@ public class ListenCreator extends SceneCreator {
 		
 		// Player 1
 		buttons = new HBox();
+		buttons.setTranslateY(100);
 		songs = players.get(0).getPlaylist().getSongs();
 		dropdown = new ChoiceBox<String>();
+		dropdown.setTranslateX((150));
+		dropdown.setTranslateY(15);
 		for (int s = 0; s < songs.size(); s++) {
 			dropdown.getItems().add(songs.get(s).getTitle() + " - " + songs.get(s).getArtist() + ".mp3");
 		}
 		dropdown.getSelectionModel().selectFirst();
-		dropdown.getStyleClass().add("playlistDropdown");
 		
 		String songName = "../resources/Songs/" + dropdown.getSelectionModel().getSelectedItem();
 	    resource = getClass().getResource(songName);
@@ -67,6 +83,7 @@ public class ListenCreator extends SceneCreator {
 	    
 	    
 	    button1 = new Button();
+	    button1.setTranslateX(205);
 	    button1.setGraphic(new ImageView(playImg));
 		button1.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
@@ -74,6 +91,7 @@ public class ListenCreator extends SceneCreator {
 			}
 		});
 		button2 = new Button();
+	    button2.setTranslateX(210);
 	    button2.setGraphic(new ImageView(pauseImg));
 		button2.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
@@ -81,6 +99,7 @@ public class ListenCreator extends SceneCreator {
 			}
 		});
 		button3 = new Button();
+	    button3.setTranslateX(215);
 	    button3.setGraphic(new ImageView(forwardImg));
 		button3.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
@@ -88,6 +107,7 @@ public class ListenCreator extends SceneCreator {
 			}
 		});
 		button4 = new Button();
+	    button4.setTranslateX(200);
 	    button4.setGraphic(new ImageView(backwardImg));
 		button4.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
@@ -100,6 +120,9 @@ public class ListenCreator extends SceneCreator {
 	    
 
 		makeGuess = new Button("Make Guess");
+		makeGuess.setAlignment(Pos.CENTER);
+		makeGuess.setTranslateX(350);
+		makeGuess.setTranslateY(200);
 		makeGuess.setOnAction(new EventHandler<ActionEvent>() {
 				 public void handle(ActionEvent e) {
 				        //set #players, #songs, theme on submit action, then ask each player for name
