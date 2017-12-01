536f58884560bcfd7f902b0a28e5ef1c9b286de3
nat82alie <nat82alie@yahoo.com>
2017-11-30 23:48:46 -0800

    added to my test suite

--- MixtapeMatcher/src/test/Natalie/NataliesTestSuite.java
+++ MixtapeMatcher/src/test/Natalie/NataliesTestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestLobby.class, TestLobbyCreator.class })
+@SuiteClasses({ TestLobby.class, TestLobbyCreator.class, TestLobbyPlayer.class })
 public class NataliesTestSuite {
 
 }
