a71c16de2418d642427f376d2298312c7989e3a3
Bridget Winn <bmwinn@calpoly.edu>
2017-11-30 20:26:16 -0800

    cleaning playlistcreator test

--- MixtapeMatcher/src/test/Matt/MattsTestSuite.java
+++ MixtapeMatcher/src/test/Matt/MattsTestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PlaylistCreatorTester.class })
+@SuiteClasses({ TestPlaylistCreator.class })
 public class MattsTestSuite {
 
 }
