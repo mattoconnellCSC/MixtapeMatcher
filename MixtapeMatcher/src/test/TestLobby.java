26c57ac8355c6b1728c78fcae26e5e5bede5ad56
nat82alie <nat82alie@yahoo.com>
2017-11-30 17:40:00 -0800

    TestLobby.java tests

--- MixtapeMatcher/src/test/TestLobby.java
+++ MixtapeMatcher/src/test/TestLobby.java
@@ -18,13 +18,46 @@ import org.junit.Test;
 public class TestLobby {
 
 	@Test
-	public void test() {
+	public void testAddPlayer() {
 		Lobby lobby = new Lobby();
 		lobby.addPlayer(new Player("Natalie"));
 		ArrayList<Player> retrievedPlayers = lobby.getPlayerList();
 		String retrievedPlayerName = retrievedPlayers.remove(0).getName();
 
 		assertSame("Natalie", retrievedPlayerName);
+	}
+	
+	@Test
+	public void testSetGetMaxSongs() {
+		int maxSongs = 3;
+		Lobby lobby = new Lobby();
+		
+		lobby.setMaxSongs(maxSongs);
+		int retreivedMaxSongs = lobby.getMaxSongs();
+
+		assertEquals(maxSongs, retreivedMaxSongs);
+	}
+	
+	@Test
+	public void testSetGetNumPlayers() {
+		int numPlayers = 3;
+		Lobby lobby = new Lobby();
+		
+		lobby.setNumPlayers(numPlayers);
+		int retreivedNumPlayers = lobby.getNumPlayers();
+
+		assertEquals(numPlayers, retreivedNumPlayers);
+	}
+	
+	@Test
+	public void testSetGetTheme() {
+		String theme = "faketheme";
+		Lobby lobby = new Lobby();
+		
+		lobby.setTheme(theme);
+		String retreivedTheme = lobby.getTheme();
+
+		assertEquals(theme, retreivedTheme);
 	}
 
 }
